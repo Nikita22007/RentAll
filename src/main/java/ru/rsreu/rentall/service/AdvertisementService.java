a8099be4f0e1816236c09853eca7654e22be12a2
Георгий Назаркин <coolgeorge41@gmail.com>
2024-06-18 17:30:48 +0300

    Добавлен контроллер для объявления (сет, гет, удаление)

--- src/main/java/ru/rsreu/rentall/service/AdvertisementService.java
+++ src/main/java/ru/rsreu/rentall/service/AdvertisementService.java
@@ -1,7 +1,13 @@
 package ru.rsreu.rentall.service;
 
 import org.springframework.stereotype.Service;
+import ru.rsreu.rentall.dto.AdvertisementDTO;
+import ru.rsreu.rentall.dto.UserDTO;
 import ru.rsreu.rentall.entity.Advertisement;
+import ru.rsreu.rentall.entity.Chat;
+import ru.rsreu.rentall.entity.User;
+import ru.rsreu.rentall.mapper.AdvertisementMapper;
+import ru.rsreu.rentall.mapper.UserMapper;
 import ru.rsreu.rentall.repository.AdvertisementRepository;
 
 import java.util.List;
@@ -13,6 +19,19 @@ public class AdvertisementService {
 
     public AdvertisementService(AdvertisementRepository advertisementRepository) {
         this.advertisementRepository = advertisementRepository;
+    }
+
+    public Advertisement createAdvertisement(AdvertisementDTO advertisementDTO) {
+        Advertisement advertisement = AdvertisementMapper.INSTANCE.toAdvertisement(advertisementDTO);
+        return advertisementRepository.save(advertisement);
+    }
+
+    public Advertisement getAdvertisementById(int id) {
+        return advertisementRepository.findById((long) id).orElse(null);
+    }
+
+    public void deleteAdvertisement(Advertisement advertisement) {
+        advertisementRepository.delete(advertisement);
     }
 
     public List<Advertisement> getAll() {
