d3854cc1016b42b3c1fc29b9b241a667cf0a83b6
killreal <crasher402@gmail.com>
2024-06-20 13:01:35 +0300

    изменен метод логина

---
+++ src/main/java/ru/tinkoff/rentall/dto/UserFullNameDTO.java
@@ -0,0 +1,11 @@
+package ru.tinkoff.rentall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class UserFullNameDTO {
+    private String userFullName;
+}
