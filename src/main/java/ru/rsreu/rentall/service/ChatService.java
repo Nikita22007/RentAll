2ef8566c3b317c6c75609796943de4be6fbac60f
Георгий Назаркин <coolgeorge41@gmail.com>
2024-06-20 10:11:11 +0300

    Instance вынесен в сервис

--- src/main/java/ru/rsreu/rentall/service/ChatService.java
+++ src/main/java/ru/rsreu/rentall/service/ChatService.java
@@ -2,7 +2,8 @@ package ru.rsreu.rentall.service;
 
 import org.springframework.stereotype.Service;
 import ru.rsreu.rentall.composite_id.ChatId;
-import ru.rsreu.rentall.entity.Chat;
+import ru.rsreu.rentall.dto.ChatDTO;
+import ru.rsreu.rentall.mapper.ChatMapper;
 import ru.rsreu.rentall.repository.ChatRepository;
 
 @Service
@@ -13,7 +14,7 @@ public class ChatService {
         this.chatRepository = chatRepository;
     }
 
-    public Chat getChatById(int id, String userOneLogin, String userTwoLogin) {
-        return chatRepository.findById(new ChatId(id, userOneLogin, userTwoLogin)).orElse(null);
+    public ChatDTO getChatById(int id, String userOneLogin, String userTwoLogin) {
+        return ChatMapper.INSTANCE.toChatDTO(chatRepository.findById(new ChatId(id, userOneLogin, userTwoLogin)).orElse(null));
     }
 }
