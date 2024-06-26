ba22335a1cdc9aa4ab27d463aa1f8e8aaf2f668a
Григорий <mr.rukodelov@gmail.com>
2024-06-27 02:06:58 +0300

    Большинство тестов, добавлена Jacoco

---
+++ src/test/java/ru/rsreu/springhelloworld/controllerTest/ImageControllerTest.java
@@ -0,0 +1,94 @@
+package ru.rsreu.springhelloworld.controllerTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.tinkoff.rentall.controller.ImageController;
+import ru.tinkoff.rentall.dto.ImageDTO;
+import ru.tinkoff.rentall.entity.Image;
+import ru.tinkoff.rentall.mapper.ImageMapper;
+import ru.tinkoff.rentall.repository.ImageRepository;
+import ru.tinkoff.rentall.service.ImageService;
+
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Disabled
+@Import({ImageController.class, ImageService.class})
+@ExtendWith(SpringExtension.class)
+public class ImageControllerTest {
+
+
+    @Autowired // сюда подставляется компонент
+    private ImageController imageController;
+
+    @MockBean
+    private ImageRepository imageRepository;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    private ImageDTO imageDTO = new ImageDTO();
+
+    private EasyRandom esr;
+
+    private EasyRandomParameters parameters;
+
+
+    @BeforeEach
+    void setUp(){
+        mockMvc = MockMvcBuilders.standaloneSetup(imageController).build();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+        objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(df);
+        this.parameters = new EasyRandomParameters()
+                .charset(StandardCharsets.UTF_8)
+                .stringLengthRange(5, 20)
+                .collectionSizeRange(1, 10);
+        this.esr = new EasyRandom(this.parameters);
+    }
+
+    @Test
+    void sendMessageTest_successful() throws Exception {
+        byte[] inputArray = "Test String".getBytes();
+//        MockMultipartFile mockMultipartFile = new MockMultipartFile("tempFileName",inputArray);
+//        Assertions.assertFalse(mockMultipartFile.isEmpty());
+//        Assertions.assertArrayEquals(inputArray, mockMultipartFile.getBytes());
+//        Assertions.assertEquals(inputArray.length,mockMultipartFile.getSize());
+//        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("src/test/java/ru/rsreu/springhelloworld/controllerTest/test.png");
+        MockMultipartFile testImage = new MockMultipartFile("file", "test.png", "image/png", inputArray);
+        imageDTO.setImgName(testImage.getBytes());
+        Image image = ImageMapper.INSTANCE.toImage(imageDTO);
+        String imageJson = objectMapper.writeValueAsString(imageDTO);
+        Mockito.doReturn(image).when(imageRepository).save(ArgumentMatchers.any());
+        mockMvc.perform(post("/image_upload").contentType(MediaType.MULTIPART_FORM_DATA_VALUE).content(imageJson))
+                .andExpect(status().isCreated());
+        verify(imageRepository, times(1)).save(ArgumentMatchers.any());
+    }
+
+}
---
+++ src/test/java/ru/rsreu/springhelloworld/testEntities/AdvertisementTestEntity.java
@@ -0,0 +1,26 @@
+package ru.rsreu.springhelloworld.testEntities;
+
+import ru.tinkoff.rentall.dto.AdvertisementDTO;
+import ru.tinkoff.rentall.entity.Advertisement;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class AdvertisementTestEntity {
+    public static Advertisement getAdvertisement(){
+        Advertisement adv = new Advertisement();
+        adv.setAdvName("Advertisement");
+        adv.setDescription("Expensive auto");
+        adv.setTimeUnit("hour");
+        adv.setRentTime(12);
+        adv.setBarterAllowed(true);
+        adv.setUser(UserTestEntity.getUser());
+        adv.setAdvPrice("5000");
+        adv.setImageId(1);
+        adv.setCategoryName("Costumes");
+        adv.setCreationTime(LocalDateTime.of(LocalDate.now(), LocalTime.now()));
+        return adv;
+    }
+}
