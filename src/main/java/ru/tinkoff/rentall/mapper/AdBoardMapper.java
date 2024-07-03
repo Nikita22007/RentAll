dbf19712cb05e8bcc064fc60e9e602fc3ba92bb1
killreal <crasher402@gmail.com>
2024-07-03 15:47:29 +0300

    оптимизировал маппер

--- src/main/java/ru/tinkoff/rentall/mapper/AdBoardMapper.java
+++ src/main/java/ru/tinkoff/rentall/mapper/AdBoardMapper.java
@@ -1,16 +1,14 @@
 package ru.tinkoff.rentall.mapper;
 
-import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ru.tinkoff.rentall.dto.AdBoardDTO;
 import ru.tinkoff.rentall.entity.Advertisement;
 
-@Mapper(uses = {UserMappingMethods.class})
 public interface AdBoardMapper {
     AdBoardMapper INSTANCE = Mappers.getMapper(AdBoardMapper.class);
-    @Mapping(target = "userLogin", source = "user", qualifiedByName = "toLogin")
+    @Mapping(target = "userLogin", source = "user.login")
     AdBoardDTO toAdBoardDTO(Advertisement advertisement);
-    @Mapping(target = "user", source = "userLogin", qualifiedByName = "fromLogin")
+    @Mapping(target = "user.login", source = "userLogin")
     Advertisement toAdvertisement(AdBoardDTO adBoardDTO);
 }
--- src/main/java/ru/tinkoff/rentall/mapper/UserReviewMapper.java
+++ src/main/java/ru/tinkoff/rentall/mapper/UserReviewMapper.java
@@ -1,20 +1,18 @@
 package ru.tinkoff.rentall.mapper;
 
-import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import ru.tinkoff.rentall.dto.UserReviewDTO;
 import ru.tinkoff.rentall.entity.UserReview;
 
-@Mapper(uses = {UserMappingMethods.class})
 public interface UserReviewMapper {
     UserReviewMapper INSTANCE = Mappers.getMapper(UserReviewMapper.class);
 
-    @Mapping(target = "userLogin", source = "userLogin", qualifiedByName = "toLogin")
-    @Mapping(target = "targetLogin", source = "targetLogin", qualifiedByName = "toLogin")
+    @Mapping(target = "userLogin", source = "userLogin.login")
+    @Mapping(target = "targetLogin", source = "targetLogin.login")
     UserReviewDTO toUserReviewDTO(UserReview userReview);
 
-    @Mapping(target = "userLogin", source = "userLogin", qualifiedByName = "fromLogin")
-    @Mapping(target = "targetLogin", source = "targetLogin", qualifiedByName = "fromLogin")
+    @Mapping(target = "userLogin.login", source = "userLogin")
+    @Mapping(target = "targetLogin.login", source = "targetLogin")
     UserReview toUserReview(UserReviewDTO userReviewDTO);
 }
